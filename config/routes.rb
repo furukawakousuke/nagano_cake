522903676ec21e1b4ddba74794da1a6105f39d3d
furukawakousuke <kendousyonan@gmail.com>
2022-09-14 12:45:40 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,28 @@
 Rails.application.routes.draw do
   
-  
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+    resources :addresses,only:[:index,:edit]
+    resources :orders,only:[:index,:show]
+    get 'orders/new'
+    get 'orders/complete'
+    resources :cart_items,only:[:index]
+    resources :customers,only:[:show,:edit]
+    get 'customers/unsubscribe'
+    resources :items,only:[:index,:show]
+    resources 
+  end
+  devise_for :admins
   namespace :admin do
+    get 'homes/new'
     resources :orders,only:[:show]
     resources :customers,only:[:index,:show,:edit]
     resources :genres,only:[:index,:edit]
     resources :items,only:[:index,:new,:show,:edit]
     
   end
-  devise_for :admins
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   
