0a86d0707681da37b5d0005173a2eeb0c1507214
furukawakousuke <kendousyonan@gmail.com>
2022-09-14 12:10:55 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  
+  
+  namespace :admin do
+    resources :orders,only:[:show]
+    resources :customers,only:[:index,:show,:edit]
+    resources :genres,only:[:index,:edit]
+    resources :items,only:[:index,:new,:show,:edit]
+    
+  end
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  devise_for :customers
+  
 end
