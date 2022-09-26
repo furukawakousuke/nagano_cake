6f243a58ab3fc6ab1f8ba7f04da7c447bf3cd53a
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2022-09-26 13:03:26 +0000

    2022/09/26

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-  
-  
-  
   scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about'
-    resources :addresses,only:[:index,:edit,:create,:destroy,:update]
-    resources :orders,only:[:index,:show]
+    resources :addresses, only: [:index,:edit,:create,:destroy,:update]
+    resources :orders, only: [:index,:show]
     get 'orders/new'
     get 'orders/complete'
-    resources :cart_items,only:[:index,:create,:update,:destroy]
-    resources :cart_items do
-  collection do
-    delete 'destroy_all'
-     end
-   end
+    resources :cart_items, only: [:index,:create,:update,:destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
     get 'customers/mypage' => 'customers#show'
     get 'customers/information/edit' => 'customers#edit'
     patch 'customers/information' => 'customers#update'
@@ -22,25 +18,26 @@ Rails.application.routes.draw do
     patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw'
     resources :items,only:[:index,:show]
   end
+
   devise_for :admin,controllers: {
     sessions: "admin/sessions"
   }
   namespace :admin do
-    
+
     root to: 'homes#top'
     resources :orders,only:[:show,:update]
     resources :customers,only:[:index,:show,:edit,:update]
     resources :genres,only:[:index,:edit,:create,:update]
     resources :items,only:[:index,:new,:show,:edit,:create,:update]
-    
+
   end
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  
+
+
   devise_for :customers,controllers:{
     registrations: "public/registrations",
   sessions: 'public/sessions'
   }
-  
+
 end
